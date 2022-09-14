5e46296c652dbf3275312b0f288ee788796d3b6e
eyelss <stud0000200014@study.utmn.ru>
2022-09-14 18:09:47 +0500

    first task

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -16,20 +17,22 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById(R.id.true_button)
         trueButton.setOnClickListener {
-            Toast.makeText(
+            val toast: Toast = Toast.makeText(
                 this,
                 R.string.correct_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+                Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
         }
 
         falseButton = findViewById(R.id.false_button)
         falseButton.setOnClickListener {
-            Toast.makeText(
+            val toast: Toast = Toast.makeText(
                 this,
                 R.string.incorrect_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+                Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
         }
     }
 }
