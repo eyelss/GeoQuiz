1778d5745a8add57ccb0cf1c7a1f82d95723a14c
eyelss <stud0000200014@study.utmn.ru>
2022-09-14 21:25:48 +0500

    fix toast gravity. second chapter

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -5,34 +5,87 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var prevButton: Button
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_text_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true),
+    )
+    private var currentIndex = 0
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnwser(userAnwser: Boolean) {
+        val correctAnwser = questionBank[currentIndex].answer
+        val messageResId = if (userAnwser == correctAnwser) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        val toast: Toast = Toast.makeText(
+            this,
+            messageResId,
+            Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.TOP or Gravity.CENTER_HORIZONTAL, 0, 0)
+        toast.show()
+    }
+
+    private fun nextQuestion() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+        updateQuestion()
+    }
+
+    private fun prevQuestion() {
+        currentIndex--
+        if(currentIndex < 0)
+            currentIndex = questionBank.size - 1
+        updateQuestion()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        questionTextView = findViewById(R.id.question_text_view)
+        questionTextView.setOnClickListener {
+            nextQuestion()
+        }
+
         trueButton = findViewById(R.id.true_button)
         trueButton.setOnClickListener {
-            val toast: Toast = Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT)
-            toast.setGravity(Gravity.TOP, 0, 0)
-            toast.show()
+            checkAnwser(true)
         }
 
         falseButton = findViewById(R.id.false_button)
         falseButton.setOnClickListener {
-            val toast: Toast = Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT)
-            toast.setGravity(Gravity.TOP, 0, 0)
-            toast.show()
+            checkAnwser(false)
         }
+
+        nextButton = findViewById(R.id.next_button)
+        nextButton.setOnClickListener {
+            nextQuestion()
+        }
+
+        prevButton = findViewById(R.id.prev_button)
+        prevButton.setOnClickListener {
+            prevQuestion()
+        }
+
+        updateQuestion()
     }
 }
